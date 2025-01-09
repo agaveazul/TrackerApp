ea2d5cb95a0caad1026bed01b334de98c773f330
Richard Strauss <rstrauss91@gmail.com>
2025-01-08 20:35:37 -0500

    increased security

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -11,7 +11,7 @@ import { isWeb } from "../utils/platform";
 
 // Get config based on environment
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
